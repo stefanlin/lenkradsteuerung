2df5721693d878efce1c06c1dbe22a73dfcea848
Stefan Lintges <stefan@lintges.de>
2021-08-11 00:51:57 +0200

    läuft

--- lenkradsteuerung/lenkradsteuerung.ino
+++ lenkradsteuerung/lenkradsteuerung.ino
@@ -1,26 +1,38 @@
 
 #include <AccelStepper.h>
-#include <Encoder.h>
+AccelStepper motorLenkrad(AccelStepper::DRIVER); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 
-Encoder handrad(5, 6);
-AccelStepper motorLenkrad; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Lenkradsteuerung Aktiv");
-  
-  motorLenkrad.setMaxSpeed(100);
-  motorLenkrad.setAcceleration(20);
 
-  handrad.write(0);
+  motorLenkrad.setMaxSpeed(8000);
+  motorLenkrad.setAcceleration(500);
+  
 }
 
-long oldPosition  = -999;
-
 void loop() {
-    long diff = handrad.read();
-    handrad.write(0);
-    Serial.println(diff);
-    motorLenkrad.move(diff);
-    motorLenkrad.run();
+  
+  motorLenkrad.run();
+  sensorValue = analogRead(analogInPin);
+  
+  outputValue = map(sensorValue, 0, 1023, -8000, 8000);
+
+  if(outputValue > 0 && outputValue < 1000){
+    outputValue = 0;
+  }
+
+  if(outputValue < 0 && outputValue > -1000){
+    outputValue = 0;
+  }
+
+  motorLenkrad.move(outputValue);
+
+  //Serial.println(outputValue);
 }
