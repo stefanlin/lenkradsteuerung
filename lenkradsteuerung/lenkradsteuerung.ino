1fe395821ab6b98c2bcf4bf4a1ee687f01e60e64
Stefan Lintges <stefan@lintges.de>
2021-11-18 17:16:43 +0100

    last version

--- lenkradsteuerung/lenkradsteuerung.ino
+++ lenkradsteuerung/lenkradsteuerung.ino
@@ -7,14 +7,15 @@ const int rotation = A0;  // turn
 const int xAxe = A1;  // left / right
 const int yAxe = A2;  // up / down
 
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
+int sensorValue = 0;
+int outputValue = 0;
+
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Lenkradsteuerung Aktiv");
 
-  motorLenkrad.setMaxSpeed(4000);
+  motorLenkrad.setMaxSpeed(8000);
   motorLenkrad.setAcceleration(8000);
   
 }
@@ -23,9 +24,9 @@ void loop() {
   const int TRIM = 1000;
   
   motorLenkrad.run();
-  sensorValue = analogRead(yAxe);
+  sensorValue = analogRead(rotation);
   
-  outputValue = map(sensorValue, 0, 1023, -4000, 4000);
+  outputValue = map(sensorValue, 0, 1023, -8000, 8000);
   motorLenkrad.run();
 
   if(outputValue > 0){
@@ -42,6 +43,4 @@ void loop() {
   motorLenkrad.run();
   motorLenkrad.move(outputValue);
   motorLenkrad.run();
-  //Serial.println(outputValue);
-
 }
